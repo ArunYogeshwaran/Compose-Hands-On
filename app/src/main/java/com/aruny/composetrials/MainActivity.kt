d3bd192883e0ef856506b47717260191da422014
ArunYogeshwaran <arun.yogeshwaran@MacBook-Pro.fritz.box>
2022-11-27 13:14:51 +0100

    Adding a surface color to the textview and using the modifier to give some padding

--- app/src/main/java/com/aruny/composetrials/MainActivity.kt
+++ app/src/main/java/com/aruny/composetrials/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -12,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.aruny.composetrials.ui.theme.ComposeHandsOnTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,7 +35,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
 }
 
 @Preview(
