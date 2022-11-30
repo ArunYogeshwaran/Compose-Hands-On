a9be672d8b541419a612506282f054b876932f0f
ArunYogeshwaran <arun.yogeshwaran@MacBook-Pro.fritz.box>
2022-11-30 10:47:48 +0100

    Adding a mutable state and changing the button text accordingly

--- app/src/main/java/com/aruny/composetrials/MainActivity.kt
+++ app/src/main/java/com/aruny/composetrials/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
@@ -46,6 +48,9 @@ private fun MyApp(
 
 @Composable
 fun Greeting(name: String) {
+    val expanded = remember {
+        mutableStateOf(false)
+    }
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -57,8 +62,8 @@ fun Greeting(name: String) {
                 Text(text = "Hello")
                 Text(text = "$name!")
             }
-            ElevatedButton(onClick = { /*TODO*/ }) {
-                Text("Show more")
+            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(if  (expanded.value) "Show less" else "Show more")
             }
         }
     }
