05aacee4e3bc82134e407196f2ef9e7ffb8657bf
ArunYogeshwaran <arun.yogeshwaran@MacBook-Pro.fritz.box>
2022-11-30 11:59:15 +0100

    Adding a spring based animation and also making sure that the app doesn't crash because of negative padding

--- app/src/main/java/com/aruny/composetrials/MainActivity.kt
+++ app/src/main/java/com/aruny/composetrials/MainActivity.kt
@@ -3,6 +3,9 @@ package com.aruny.composetrials
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -84,7 +87,13 @@ fun Greeting(name: String) {
     val expanded = rememberSaveable {
         mutableStateOf(false)
     }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        targetValue = if (expanded.value) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = MaterialTheme.colorScheme.primary,
@@ -94,12 +103,14 @@ fun Greeting(name: String) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello")
                 Text(text = "$name!")
             }
-            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
+            ElevatedButton(
+                onClick = { expanded.value = !expanded.value }
+            ) {
                 Text(if (expanded.value) "Show less" else "Show more")
             }
         }
